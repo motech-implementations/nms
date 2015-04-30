d06530e4f9a208efe89867a32b976550a588b06f
Lauren <llavoie@grameenfoundation.org>
2015-04-30 12:48:12 -0700

    Fixes #168 Save Deactivation Reason
    Adds a DeactivationReason enum and passes it to deactivateSubscription method

--- kilkari/src/main/java/org/motechproject/nms/kilkari/service/SubscriptionService.java
+++ kilkari/src/main/java/org/motechproject/nms/kilkari/service/SubscriptionService.java
@@ -1,10 +1,11 @@
 package org.motechproject.nms.kilkari.service;
 
+import org.motechproject.nms.kilkari.domain.DeactivationReason;
 import org.motechproject.nms.kilkari.domain.InboxCallDetails;
 import org.motechproject.nms.kilkari.domain.Subscriber;
 import org.motechproject.nms.kilkari.domain.Subscription;
-import org.motechproject.nms.kilkari.domain.SubscriptionPack;
 import org.motechproject.nms.kilkari.domain.SubscriptionMode;
+import org.motechproject.nms.kilkari.domain.SubscriptionPack;
 
 /**
  *
@@ -18,7 +19,7 @@ public interface SubscriptionService {
 
     Subscription getSubscription(String subscriptionId);
 
-    void deactivateSubscription(Subscription subscription);
+    void deactivateSubscription(Subscription subscription, DeactivationReason reason);
 
     SubscriptionPack getSubscriptionPack(String name);
 
