f63d3136c7bf6787738ee4cb66dbf088ccd3f495
Frank Huster <fhuster@grameenfoundation.org>
2015-05-11 21:19:17 -0700

    Removed yet more test code I should've deleted before committing

--- imi/src/test/java/org/motechproject/nms/imi/unit/StatusCodeTest.java
+++ imi/src/test/java/org/motechproject/nms/imi/unit/StatusCodeTest.java
@@ -1,10 +1,7 @@
 package org.motechproject.nms.imi.unit;
 
-import org.joda.time.DateTime;
 import org.junit.Test;
 import org.motechproject.nms.imi.domain.StatusCode;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import static junit.framework.Assert.assertEquals;
 
@@ -23,14 +20,5 @@ public class StatusCodeTest {
     @Test(expected=IllegalArgumentException.class)
     public void testFromIntFailure() {
         StatusCode.fromInt(0);
-    }
-
-    @Test
-    public void testDayOfWeek() {
-        Logger logger = LoggerFactory.getLogger(StatusCodeTest.class);
-        DateTime today = DateTime.now();
-        for (int i=0 ; i<=7 ; i++) {
-            logger.debug("today.plusDays({}).getDayOfWeek() = {}", i, today.plusDays(i).getDayOfWeek());
-        }
     }
 }
