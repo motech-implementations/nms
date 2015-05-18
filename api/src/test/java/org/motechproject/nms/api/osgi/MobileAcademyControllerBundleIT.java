7b982a78bc2a211db364532b189d33c0c05b6425
Rob LaRubbio <rob@larubbio.org>
2015-05-17 20:41:38 -0700

    merge origin/master

--- api/src/test/java/org/motechproject/nms/api/osgi/MobileAcademyControllerBundleIT.java
+++ api/src/test/java/org/motechproject/nms/api/osgi/MobileAcademyControllerBundleIT.java
@@ -13,7 +13,6 @@ import org.motechproject.nms.api.web.contract.mobileAcademy.CourseResponse;
 import org.motechproject.nms.api.web.contract.mobileAcademy.SaveBookmarkRequest;
 import org.motechproject.nms.api.web.converter.MobileAcademyConverter;
 import org.motechproject.nms.mobileacademy.domain.Course;
-import org.motechproject.nms.mobileacademy.exception.CourseNotCompletedException;
 import org.motechproject.nms.mobileacademy.service.MobileAcademyService;
 import org.motechproject.testing.osgi.BasePaxIT;
 import org.motechproject.testing.osgi.container.MotechNativeTestContainerFactory;
--- kilkari/src/test/java/org/motechproject/nms/kilkari/osgi/SubscriptionServiceBundleIT.java
+++ kilkari/src/test/java/org/motechproject/nms/kilkari/osgi/SubscriptionServiceBundleIT.java
@@ -46,10 +46,7 @@ import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 /**
  * Verify that SubscriptionService is present & functional.
