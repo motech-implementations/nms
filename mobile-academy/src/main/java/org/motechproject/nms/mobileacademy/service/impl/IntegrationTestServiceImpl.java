1c91bf256e64bc43ab57b393cbf55e84e48417a2
Frank Huster <fhuster@grameenfoundation.org>
2015-05-29 09:17:37 -0700

    No longer deleting course table

--- mobile-academy/src/main/java/org/motechproject/nms/mobileacademy/service/impl/IntegrationTestServiceImpl.java
+++ mobile-academy/src/main/java/org/motechproject/nms/mobileacademy/service/impl/IntegrationTestServiceImpl.java
@@ -1,7 +1,6 @@
 package org.motechproject.nms.mobileacademy.service.impl;
 
 import org.motechproject.nms.mobileacademy.repository.CompletionRecordDataService;
-import org.motechproject.nms.mobileacademy.repository.NmsCourseDataService;
 import org.motechproject.nms.mobileacademy.service.IntegrationTestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,12 +10,9 @@ public class IntegrationTestServiceImpl implements IntegrationTestService {
 
     @Autowired
     private CompletionRecordDataService completionRecordDataService;
-    @Autowired
-    private NmsCourseDataService courseDataService;
 
 
     public void deleteAll() {
         completionRecordDataService.deleteAll();
-        courseDataService.deleteAll();
     }
 }
