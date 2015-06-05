e57eb1df6e5c27c7a340a959a87fe77884dbea72
priyajain5 <priya5.jain@aricent.com>
2015-06-05 12:22:39 +0530

    forgot to use @Ignore

--- testing/src/test/java/org/motechproject/nms/testing/it/kilkari/InboxServiceBundleIT.java
+++ testing/src/test/java/org/motechproject/nms/testing/it/kilkari/InboxServiceBundleIT.java
@@ -8,6 +8,7 @@ import java.util.Set;
 import javax.inject.Inject;
 
 import org.joda.time.DateTime;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.motechproject.nms.kilkari.domain.Subscriber;
@@ -16,12 +17,7 @@ import org.motechproject.nms.kilkari.domain.SubscriptionOrigin;
 import org.motechproject.nms.kilkari.domain.SubscriptionPack;
 import org.motechproject.nms.kilkari.domain.SubscriptionPackMessage;
 import org.motechproject.nms.kilkari.domain.SubscriptionPackType;
-import org.motechproject.nms.kilkari.repository.InboxCallDataDataService;
-import org.motechproject.nms.kilkari.repository.InboxCallDetailRecordDataService;
-import org.motechproject.nms.kilkari.repository.SubscriberDataService;
-import org.motechproject.nms.kilkari.repository.SubscriptionDataService;
 import org.motechproject.nms.kilkari.repository.SubscriptionPackDataService;
-import org.motechproject.nms.kilkari.repository.SubscriptionPackMessageDataService;
 import org.motechproject.nms.kilkari.service.InboxService;
 import org.motechproject.nms.kilkari.service.SubscriberService;
 import org.motechproject.nms.kilkari.service.SubscriptionService;
@@ -30,8 +26,6 @@ import org.motechproject.nms.region.domain.District;
 import org.motechproject.nms.region.domain.Language;
 import org.motechproject.nms.region.domain.LanguageLocation;
 import org.motechproject.nms.region.domain.State;
-import org.motechproject.nms.region.repository.CircleDataService;
-import org.motechproject.nms.region.repository.DistrictDataService;
 import org.motechproject.nms.region.repository.LanguageDataService;
 import org.motechproject.nms.region.repository.LanguageLocationDataService;
 import org.motechproject.nms.region.repository.StateDataService;
@@ -126,6 +120,7 @@ public class InboxServiceBundleIT extends BasePaxIT {
 	 * This test case covers the scenario for NMS_FT_108, NMS_FT_121  
 	 */
 	@Test
+	@Ignore
 	public void verifyFT108_121() throws Exception {
 
 		/*
