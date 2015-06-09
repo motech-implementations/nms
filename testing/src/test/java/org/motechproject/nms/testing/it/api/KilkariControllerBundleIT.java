af69903d2f83864c6b082c968c8ce9cdaea5650b
AnkitGaurNms <ankit.gaur@aricent.com>
2015-06-09 17:43:38 +0530

    Implemented FT cases 117, 118

--- testing/src/test/java/org/motechproject/nms/testing/it/api/KilkariControllerBundleIT.java
+++ testing/src/test/java/org/motechproject/nms/testing/it/api/KilkariControllerBundleIT.java
@@ -773,5 +773,116 @@ public class KilkariControllerBundleIT extends BasePaxIT {
         assertTrue(expectedJsonResponse.equals(EntityUtils.toString(response.getEntity()))  );
 
     }
-    
+
+    @Test
+    public void verifyFT117() throws IOException, InterruptedException {
+        /**
+         * To check NMS is able to make available a single message of current
+         * week in inbox with single message per week configuration . when a)
+         * user's MSISDN is subscribed for 48Weeks Pack. b)user's MSISDN is
+         * deactivated for an old subscription of 72Weeks Pack.
+         **/
+        // setup data to remove 2 messages per week configuration for 72 week pack
+        testingService.clearDatabase();
+        rh = new RegionHelper(languageDataService, circleDataService,
+                stateDataService, districtDataService);
+
+        sh = new SubscriptionHelper(subscriptionService, subscriberDataService,
+                subscriptionPackDataService, languageDataService,
+                circleDataService, stateDataService, districtDataService);
+
+        deployedServiceDataService.create(new DeployedService(rh.delhiState(),
+                Service.KILKARI));
+
+        Subscriber mctsSubscriber = new Subscriber(9999911122L);
+        mctsSubscriber.setDateOfBirth(null);
+        // set LMP for pregnancy pack
+        mctsSubscriber.setLastMenstrualPeriod(DateTime.now().minusDays(240));
+        subscriberDataService.create(mctsSubscriber);
+
+        // create old subscription for pregnancy pack and deactivate it
+        Subscription oldSubscription = subscriptionService.createSubscription(
+                9999911122L, rh.hindiLanguage(), sh.pregnancyPack(1),
+                SubscriptionOrigin.MCTS_IMPORT);
+        subscriptionService.deactivateSubscription(oldSubscription,
+                DeactivationReason.DEACTIVATED_BY_USER);
+
+
+        mctsSubscriber = subscriberDataService.findByCallingNumber(9999911122L);
+
+        // create new subscription for child pack in Active state such that
+        // next OBD date falls on current date
+        mctsSubscriber.setLastMenstrualPeriod(null);
+        mctsSubscriber.setDateOfBirth(DateTime.now());
+        subscriberDataService.update(mctsSubscriber);
+        Subscription newSubscription = subscriptionService.createSubscription(
+                9999911122L, rh.hindiLanguage(), sh.childPack(),
+                SubscriptionOrigin.MCTS_IMPORT);
+
+        String expectedJsonResponse = "{\"inboxSubscriptionDetailList\":[{\"subscriptionId\":\""
+                + newSubscription.getSubscriptionId()
+                + "\",\"subscriptionPack\":\"childPack\",\"inboxWeekId\":\"w1_1\",\"contentFileName\":\"w1_1.wav\"}]}";
+
+        HttpGet httpGet = createHttpGet(true, "9999911122", true,
+                "123456789012345");
+        assertTrue(SimpleHttpClient.execHttpRequest(httpGet, HttpStatus.SC_OK,
+                expectedJsonResponse, ADMIN_USERNAME, ADMIN_PASSWORD));
+    }
+
+    @Test
+    public void verifyFT118() throws IOException, InterruptedException {
+        /**
+         * To check NMS is able to make available a single message of current
+         * week in inbox with single message per week configuration . when a)
+         * user's MSISDN is subscribed for 48Weeks Pack. b)user's MSISDN status
+         * is completed for an old subscription of 72Weeks Pack.
+         **/
+        // setup data to remove 2 messages per week configuration for 72 week
+        // pack
+        testingService.clearDatabase();
+        rh = new RegionHelper(languageDataService, circleDataService,
+                stateDataService, districtDataService);
+
+        sh = new SubscriptionHelper(subscriptionService, subscriberDataService,
+                subscriptionPackDataService, languageDataService,
+                circleDataService, stateDataService, districtDataService);
+
+        deployedServiceDataService.create(new DeployedService(rh.delhiState(),
+                Service.KILKARI));
+
+        Subscriber mctsSubscriber = new Subscriber(9999911122L);
+        mctsSubscriber.setDateOfBirth(null);
+        // set LMP for pregnancy pack
+        mctsSubscriber.setLastMenstrualPeriod(DateTime.now().minusDays(240));
+        subscriberDataService.create(mctsSubscriber);
+
+        // create old subscription for pregnancy pack
+        Subscription oldSubscription = subscriptionService.createSubscription(
+                9999911122L, rh.hindiLanguage(), sh.pregnancyPack(1),
+                SubscriptionOrigin.MCTS_IMPORT);
+        // update old pregnancy subscription pack to mark complete setting the
+        // subscription to have ended more than a week ago
+        subscriptionService.updateStartDate(oldSubscription, DateTime.now()
+                .minusDays(512 + 90));
+
+        mctsSubscriber = subscriberDataService.findByCallingNumber(9999911122L);
+
+        // create new subscription for child pack in Active state such that
+        // next OBD date falls on current date
+        mctsSubscriber.setLastMenstrualPeriod(null);
+        mctsSubscriber.setDateOfBirth(DateTime.now());
+        subscriberDataService.update(mctsSubscriber);
+        Subscription newSubscription = subscriptionService.createSubscription(
+                9999911122L, rh.hindiLanguage(), sh.childPack(),
+                SubscriptionOrigin.MCTS_IMPORT);
+
+        String expectedJsonResponse = "{\"inboxSubscriptionDetailList\":[{\"subscriptionId\":\""
+                + newSubscription.getSubscriptionId()
+                + "\",\"subscriptionPack\":\"childPack\",\"inboxWeekId\":\"w1_1\",\"contentFileName\":\"w1_1.wav\"}]}";
+
+        HttpGet httpGet = createHttpGet(true, "9999911122", true,
+                "123456789012345");
+        assertTrue(SimpleHttpClient.execHttpRequest(httpGet, HttpStatus.SC_OK,
+                expectedJsonResponse, ADMIN_USERNAME, ADMIN_PASSWORD));
+    }
 }
