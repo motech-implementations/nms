78710e8f58ae48915664bbb0e46456883ac5ed44
Frank Huster <fhuster@grameenfoundation.org>
2015-06-08 22:22:39 -0700

    CR feedback

--- region/src/main/java/org/motechproject/nms/region/service/impl/LanguageServiceImpl.java
+++ region/src/main/java/org/motechproject/nms/region/service/impl/LanguageServiceImpl.java
@@ -57,7 +57,7 @@ public class LanguageServiceImpl implements LanguageService {
                         "from nms_languages l " +
                         "join nms_districts d on  d.language_id_oid = l.id " +
                         "join nms_states s on d.state_id_oid = s.id " +
-                        "join nms_states_circles sc on s.id = sc.state_id and sc.circle_id = ?";
+                        "join nms_states_join_circles sxc on s.id = sxc.state_id and sxc.circle_id = ?";
 
                 return query;
             }
@@ -69,12 +69,7 @@ public class LanguageServiceImpl implements LanguageService {
             }
         };
 
-        List<Language> languages;
-        try {
-            languages = languageDataService.executeSQLQuery(queryExecution);
-        } catch (Exception e) {
-            return null;
-        }
+        List<Language> languages = languageDataService.executeSQLQuery(queryExecution);
 
         if (languages == null) {
             languages = new ArrayList<>();
