8f2587469206e66f63a19d8b576b89caf4b8eac5
srivastavaAbhi <abhisrivastava.17016@gmail.com>
2015-06-17 13:21:43 -0700

    NMS.FT.193.194.195
    Incorporated @koshalt comments

--- testing/src/test/java/org/motechproject/nms/testing/it/imi/ImiControllerObdBundleIT.java
+++ testing/src/test/java/org/motechproject/nms/testing/it/imi/ImiControllerObdBundleIT.java
@@ -31,7 +31,6 @@ import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
 import org.ops4j.pax.exam.spi.reactors.PerSuite;
 
 import javax.inject.Inject;
-import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
@@ -41,7 +40,7 @@ import static org.junit.Assert.assertTrue;
 @RunWith(PaxExam.class)
 @ExamReactorStrategy(PerSuite.class)
 @ExamFactory(MotechNativeTestContainerFactory.class)
-public class ImiController_OBD_BundleIT extends BasePaxIT {
+public class ImiControllerObdBundleIT extends BasePaxIT {
 
     private String localObdDirBackup;
     private String remoteObdDirBackup;
@@ -184,14 +183,55 @@ public class ImiController_OBD_BundleIT extends BasePaxIT {
     /*
     * NMS_FT_193 :To check "NotifyFileProcessedStatus" API is rejected in case the file could not
     * be copied or the file is not available.
-    * NMS_FT_194 : To check "NotifyFileProcessedStatus" API is rejected in case there is an error in checksum
-    * NMS_FT_195 : To check "NotifyFileProcessedStatus" API is rejected in case there is an error in records check.
     */
     @Test
-    public void verifyFT193_194_195() throws IOException, InterruptedException {
+    public void verifyFT193() throws IOException, InterruptedException {
         getLogger().debug("testCreateFileProcessedStatusRequestWithErrorFILE_NOT_ACCESSIBLE()");
         HttpPost httpPost = createFileProcessedStatusHttpPost("file.csv",
                 FileProcessedStatus.FILE_NOT_ACCESSIBLE);
+
+        fileAuditRecordDataService.create(new FileAuditRecord(FileType.TARGET_FILE, "file.csv", false, "ERROR",
+                null, null));
+
+        assertTrue(SimpleHttpClient.execHttpRequest(httpPost, HttpStatus.SC_OK, ImiTestHelper.ADMIN_USERNAME, ImiTestHelper.ADMIN_PASSWORD));
+
+        //check an alert was sent
+        AlertCriteria criteria = new AlertCriteria().byExternalId("file.csv");
+        List<Alert> alerts = alertService.search(criteria);
+        assertEquals(1, alerts.size());
+        assertEquals(AlertType.CRITICAL, alerts.get(0).getAlertType());
+    }
+
+    /*
+    * NMS_FT_194 : To check "NotifyFileProcessedStatus" API is rejected in case there is an error in checksum
+    */
+    @Test
+    public void verifyFT194() throws IOException, InterruptedException {
+        getLogger().debug("testCreateFileProcessedStatusRequestWithErrorFILE_CHECKSUM_ERROR()");
+        HttpPost httpPost = createFileProcessedStatusHttpPost("file.csv",
+                FileProcessedStatus.FILE_CHECKSUM_ERROR);
+
+        fileAuditRecordDataService.create(new FileAuditRecord(FileType.TARGET_FILE, "file.csv", false, "ERROR",
+                null, null));
+
+        assertTrue(SimpleHttpClient.execHttpRequest(httpPost, HttpStatus.SC_OK, ImiTestHelper.ADMIN_USERNAME, ImiTestHelper.ADMIN_PASSWORD));
+
+        //check an alert was sent
+        AlertCriteria criteria = new AlertCriteria().byExternalId("file.csv");
+        List<Alert> alerts = alertService.search(criteria);
+        assertEquals(1, alerts.size());
+        assertEquals(AlertType.CRITICAL, alerts.get(0).getAlertType());
+    }
+
+    /*
+
+    * NMS_FT_195 : To check "NotifyFileProcessedStatus" API is rejected in case there is an error in records check.
+    */
+    @Test
+    public void verifyFT195() throws IOException, InterruptedException {
+        getLogger().debug("testCreateFileProcessedStatusRequestWithErrorFILE_RECORDSCOUNT_ERROR()");
+        HttpPost httpPost = createFileProcessedStatusHttpPost("file.csv",
+                FileProcessedStatus.FILE_RECORDSCOUNT_ERROR);
 
         fileAuditRecordDataService.create(new FileAuditRecord(FileType.TARGET_FILE, "file.csv", false, "ERROR",
                 null, null));
