eddc7766e452dcbbe2f37fdb6bc4d54b26bc25dd
Frank Huster <fhuster@grameenfoundation.org>
2015-07-08 11:46:05 -0700

    Some fixes, @kosh to take over

--- testing/src/test/java/org/motechproject/nms/testing/it/api/MobileAcademyControllerBundleIT.java
+++ testing/src/test/java/org/motechproject/nms/testing/it/api/MobileAcademyControllerBundleIT.java
@@ -223,8 +223,7 @@ public class MobileAcademyControllerBundleIT extends BasePaxIT {
      * To verify Get MA Course Version API is returning correct course version
      * when MA course structure exist .
      */
-    // TODO https://applab.atlassian.net/browse/NMS-226
-    @Ignore
+    // https://applab.atlassian.net/browse/NMS-226
     @Test
     public void verifyFT401() throws IOException, InterruptedException {
         setupMaCourse();
@@ -271,8 +270,7 @@ public class MobileAcademyControllerBundleIT extends BasePaxIT {
      * To verify Get MA Course API is returning correct course structure when MA
      * course structure exist.
      */
-    // TODO https://applab.atlassian.net/browse/NMS-227
-    @Ignore
+    // https://applab.atlassian.net/browse/NMS-227
     @Test
     public void verifyFT403() throws IOException, InterruptedException {
         JSONObject jo = setupMaCourse();
