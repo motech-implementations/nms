0ca6b9b5207b1ce0c5c010a60b329900bbe2e703
Rob LaRubbio <rob@larubbio.org>
2015-07-16 13:45:05 -0700

    Fixes NIP-72
    When we create a subscription we now set the first and second message date.  When we load
    subscriptions at target generation time we decide if we should load only the first message day or first and second message day.

--- kilkari/src/main/java/org/motechproject/nms/kilkari/repository/SubscriptionDataService.java
+++ kilkari/src/main/java/org/motechproject/nms/kilkari/repository/SubscriptionDataService.java
@@ -8,25 +8,12 @@ import org.motechproject.mds.service.MotechDataService;
 import org.motechproject.mds.util.Constants;
 import org.motechproject.nms.kilkari.domain.Subscription;
 import org.motechproject.nms.kilkari.domain.SubscriptionStatus;
-import org.motechproject.nms.props.domain.DayOfTheWeek;
 
 import java.util.List;
 
 public interface SubscriptionDataService extends MotechDataService<Subscription> {
     @Lookup
     Subscription findBySubscriptionId(@LookupField(name = "subscriptionId") String subscriptionId);
-
-    @Lookup
-    List<Subscription> findByStatusAndFirstMessageDayOfWeek(
-            @LookupField(name = "status") SubscriptionStatus status,
-            @LookupField(name = "firstMessageDayOfWeek") DayOfTheWeek startDayOfTheWeek,
-            QueryParams queryParams);
-
-    @Lookup
-    List<Subscription> findByStatusAndSecondMessageDayOfWeek(
-            @LookupField(name = "status") SubscriptionStatus status,
-            @LookupField(name = "secondMessageDayOfWeek") DayOfTheWeek startDayOfTheWeek,
-            QueryParams queryParams);
 
     @Lookup
     List<Subscription> findByStatus(@LookupField(name = "status")SubscriptionStatus status);
