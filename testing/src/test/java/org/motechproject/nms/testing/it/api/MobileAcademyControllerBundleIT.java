9c900ddf8a99329e74a233b9ab55c2a6e374e9f2
AnkitGaurNms <ankit.gaur@aricent.com>
2015-07-28 17:53:22 +0530

    Implemented FT 562, 563

--- testing/src/test/java/org/motechproject/nms/testing/it/api/MobileAcademyControllerBundleIT.java
+++ testing/src/test/java/org/motechproject/nms/testing/it/api/MobileAcademyControllerBundleIT.java
@@ -1,7 +1,6 @@
 package org.motechproject.nms.testing.it.api;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
@@ -9,6 +8,7 @@ import static org.junit.Assert.assertTrue;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.inject.Inject;
@@ -26,6 +26,9 @@ import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.motechproject.alerts.contract.AlertCriteria;
+import org.motechproject.alerts.contract.AlertService;
+import org.motechproject.alerts.domain.Alert;
 import org.motechproject.mtraining.service.BookmarkService;
 import org.motechproject.nms.api.web.BaseController;
 import org.motechproject.nms.api.web.contract.BadRequest;
@@ -34,6 +37,7 @@ import org.motechproject.nms.api.web.contract.mobileAcademy.SaveBookmarkRequest;
 import org.motechproject.nms.api.web.contract.mobileAcademy.SmsStatusRequest;
 import org.motechproject.nms.api.web.contract.mobileAcademy.sms.DeliveryStatus;
 import org.motechproject.nms.api.web.contract.mobileAcademy.sms.RequestData;
+import org.motechproject.nms.imi.service.SettingsService;
 import org.motechproject.nms.mobileacademy.domain.CompletionRecord;
 import org.motechproject.nms.mobileacademy.domain.NmsCourse;
 import org.motechproject.nms.mobileacademy.dto.MaCourse;
@@ -41,6 +45,7 @@ import org.motechproject.nms.mobileacademy.repository.CompletionRecordDataServic
 import org.motechproject.nms.mobileacademy.repository.NmsCourseDataService;
 import org.motechproject.nms.testing.it.api.utils.RequestBuilder;
 import org.motechproject.nms.testing.service.TestingService;
+import org.motechproject.server.config.SettingsFacade;
 import org.motechproject.testing.osgi.BasePaxIT;
 import org.motechproject.testing.osgi.container.MotechNativeTestContainerFactory;
 import org.motechproject.testing.osgi.http.SimpleHttpClient;
@@ -75,6 +80,12 @@ public class MobileAcademyControllerBundleIT extends BasePaxIT {
     private static final String FINAL_BOOKMARK = "COURSE_COMPLETED";
 
     public static final int MILLISECONDS_PER_SECOND = 1000;
+
+    @Inject
+    private SettingsService settingsService;
+
+    @Inject
+    private AlertService alertService;
 
     @Before
     public void setupTestData() {
@@ -1137,5 +1148,118 @@ public class MobileAcademyControllerBundleIT extends BasePaxIT {
                 cr.getLastDeliveryStatus());
 
     }
+
+    /**
+     * To verify SMS is sent successfully after FLW has completed the course.
+     */
+    @Test
+    public void verifyFT562() throws IOException, InterruptedException {
+        SettingsFacade settingsFacade = settingsService.getSettingsFacade();
+
+        String SMS_NOTIFICATION_URL = "imi.sms.notification.url";
+        String oldUrl = settingsFacade.getProperty(SMS_NOTIFICATION_URL);
+        String newUrl = String.format("http://localhost:%d/testing/sendSMS200",
+                TestContext.getJettyPort());
+        settingsFacade.setProperty(SMS_NOTIFICATION_URL, newUrl);
+
+        String endpoint = String.format(
+                "http://localhost:%d/api/mobileacademy/bookmarkWithScore",
+                TestContext.getJettyPort());
+        SaveBookmarkRequest bookmarkRequest = new SaveBookmarkRequest();
+
+        // Invoke save bookmark for course completion
+        bookmarkRequest.setCallingNumber(1234567890l);
+        bookmarkRequest.setCallId(123456789012345l);
+        bookmarkRequest.setBookmark("COURSE_COMPLETED");
+        Map<String, Integer> scoreMap = new HashMap<String, Integer>();
+        scoreMap.put("1", 2);
+        scoreMap.put("2", 1);
+        scoreMap.put("3", 4);
+        scoreMap.put("4", 2);
+        scoreMap.put("5", 1);
+        scoreMap.put("6", 4);
+        scoreMap.put("7", 2);
+        scoreMap.put("8", 1);
+        scoreMap.put("9", 4);
+        scoreMap.put("10", 1);
+        scoreMap.put("11", 4);
+        bookmarkRequest.setScoresByChapter(scoreMap);
+        HttpPost postRequest = RequestBuilder.createPostRequest(endpoint,
+                bookmarkRequest);
+        assertTrue(SimpleHttpClient.execHttpRequest(postRequest,
+                HttpStatus.SC_OK, RequestBuilder.ADMIN_USERNAME,
+                RequestBuilder.ADMIN_PASSWORD));
+
+        Thread.currentThread().sleep(5000);
+
+        CompletionRecord cr = completionRecordDataService
+                .findRecordByCallingNumber(1234567890l);
+        assertNotNull(cr);
+        assertEquals(true, cr.isSentNotification());
+
+        settingsFacade.setProperty(SMS_NOTIFICATION_URL, oldUrl);
+    }
+
+    /**
+     * To verify SMS sending is retried when first delivery attempt failed.
+     */
+    @Test
+    public void verifyFT563() throws IOException, InterruptedException {
+        SettingsFacade settingsFacade = settingsService.getSettingsFacade();
+
+        String SMS_NOTIFICATION_URL = "imi.sms.notification.url";
+        String oldUrl = settingsFacade.getProperty(SMS_NOTIFICATION_URL);
+
+        // Set Invalid IMI URL so that it 'll retry to send SMS
+        String newUrl = String.format("http://localhost:%d/testing/invalid",
+                TestContext.getJettyPort());
+        settingsFacade.setProperty(SMS_NOTIFICATION_URL, newUrl);
+
+        String endpoint = String.format(
+                "http://localhost:%d/api/mobileacademy/bookmarkWithScore",
+                TestContext.getJettyPort());
+        SaveBookmarkRequest bookmarkRequest = new SaveBookmarkRequest();
+
+        // save bookmark for course completion
+        bookmarkRequest.setCallingNumber(1234567890l);
+        bookmarkRequest.setCallId(123456789012345l);
+        bookmarkRequest.setBookmark("COURSE_COMPLETED");
+        Map<String, Integer> scoreMap = new HashMap<String, Integer>();
+        scoreMap.put("1", 2);
+        scoreMap.put("2", 1);
+        scoreMap.put("3", 4);
+        scoreMap.put("4", 2);
+        scoreMap.put("5", 1);
+        scoreMap.put("6", 4);
+        scoreMap.put("7", 2);
+        scoreMap.put("8", 1);
+        scoreMap.put("9", 4);
+        scoreMap.put("10", 1);
+        scoreMap.put("11", 4);
+        bookmarkRequest.setScoresByChapter(scoreMap);
+        HttpPost postRequest = RequestBuilder.createPostRequest(endpoint,
+                bookmarkRequest);
+        assertTrue(SimpleHttpClient.execHttpRequest(postRequest,
+                HttpStatus.SC_OK, RequestBuilder.ADMIN_USERNAME,
+                RequestBuilder.ADMIN_PASSWORD));
+
+        Thread.currentThread().sleep(10000);
+
+        CompletionRecord cr = completionRecordDataService
+                .findRecordByCallingNumber(1234567890l);
+        assertNotNull(cr);
+        assertEquals(true, cr.isSentNotification());
+
+        // assert alert service records for retry
+        int maxRetryCount = Integer.parseInt(settingsFacade
+                .getProperty("imi.notification_retry_count"));
+        List<Alert> retryAlerts = alertService.search(new AlertCriteria()
+                .byExternalId("SmsNotification"));
+        assertTrue(maxRetryCount >= 1);
+        // +1 for first attempt
+        assertTrue(retryAlerts.size() == maxRetryCount + 1);
+        settingsFacade.setProperty(SMS_NOTIFICATION_URL, oldUrl);
+    }
+
 }
 
