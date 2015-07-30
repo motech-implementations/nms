46f1ba4a0132906ab9aefb1f2b869adc87e9a5da
Koshal Thirumalai <kosh@koshal.org>
2015-07-30 11:25:50 -0700

    Standardizing on finalCallStatus

--- flw/src/main/java/org/motechproject/nms/flw/domain/CallDetailRecord.java
+++ flw/src/main/java/org/motechproject/nms/flw/domain/CallDetailRecord.java
@@ -5,7 +5,6 @@ import org.joda.time.DateTime;
 import org.motechproject.mds.annotations.Cascade;
 import org.motechproject.mds.annotations.Entity;
 import org.motechproject.mds.annotations.Field;
-import org.motechproject.mds.annotations.Ignore;
 import org.motechproject.nms.props.domain.CallDisconnectReason;
 import org.motechproject.nms.props.domain.FinalCallStatus;
 import org.motechproject.nms.props.domain.Service;
@@ -174,12 +173,6 @@ public class CallDetailRecord {
 
     public void setFinalCallStatus(FinalCallStatus finalCallStatus) {
         this.finalCallStatus = finalCallStatus;
-    }
-
-    @Ignore
-    public void setCallStatus(int i) {
-
-        this.finalCallStatus = FinalCallStatus.fromInt(i);
     }
 
     public CallDisconnectReason getCallDisconnectReason() {
