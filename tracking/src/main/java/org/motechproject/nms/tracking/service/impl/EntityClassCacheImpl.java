e4b3e5dd78bed08af2ecb7bf6c1aae90d76353a4
Frank Huster <fhuster@grameenfoundation.org>
2015-09-15 16:59:13 -0700

    Should build...

--- tracking/src/main/java/org/motechproject/nms/tracking/service/impl/EntityClassCacheImpl.java
+++ tracking/src/main/java/org/motechproject/nms/tracking/service/impl/EntityClassCacheImpl.java
@@ -2,13 +2,10 @@ package org.motechproject.nms.tracking.service.impl;
 
 import org.motechproject.mds.service.EntityService;
 import org.motechproject.nms.tracking.service.EntityClassCache;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class EntityClassCacheImpl implements EntityClassCache {
     private EntityService entityService;
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(EntityClassCacheImpl.class);
 
     public EntityClassCacheImpl(EntityService entityService) {
         this.entityService = entityService;
