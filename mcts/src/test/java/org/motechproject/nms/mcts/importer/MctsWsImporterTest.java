2a504ff8ce32385e8522b75f3c933fb7477ac010
Paweł Gesek <pgesek@soldevelo.com>
2015-10-28 16:52:15 +0100

    Extended unit test with mother import

--- mcts/src/test/java/org/motechproject/nms/mcts/importer/MctsWsImporterTest.java
+++ mcts/src/test/java/org/motechproject/nms/mcts/importer/MctsWsImporterTest.java
@@ -24,6 +24,7 @@ import org.motechproject.nms.mcts.contract.AnmAshaDataSet;
 import org.motechproject.nms.mcts.contract.AnmAshaRecord;
 import org.motechproject.nms.mcts.contract.ChildRecord;
 import org.motechproject.nms.mcts.contract.ChildrenDataSet;
+import org.motechproject.nms.mcts.contract.MotherRecord;
 import org.motechproject.nms.mcts.contract.MothersDataSet;
 import org.motechproject.nms.mcts.service.MctsWebServiceFacade;
 import org.motechproject.nms.mcts.utils.Constants;
@@ -84,6 +85,8 @@ public class MctsWsImporterTest {
     private AnmAshaDataSet flwDs2;
     private ChildrenDataSet childDs1;
     private ChildrenDataSet childDs2;
+    private MothersDataSet motherDs1;
+    private MothersDataSet motherDs2;
 
     private final LocalDate today = DateUtil.today();
     private final LocalDate yesterday = today.minusDays(1);
@@ -118,7 +121,7 @@ public class MctsWsImporterTest {
         prepStates();
         prepFlwData();
         prepChildData();
-        prepEmtyImport(); // TODO: remove after child and mother import is added
+        prepMotherData();
         when(settingsFacade.getProperty(Constants.MCTS_ENDPOINT)).thenReturn("http://localhost/web-service");
         when(settingsFacade.getProperty(Constants.MCTS_LOCATIONS)).thenReturn("1,15");
         when(stateDataService.findByCode(1L)).thenReturn(state1);
@@ -133,6 +136,11 @@ public class MctsWsImporterTest {
                 .thenReturn(childDs1);
         when(mctsWebServiceFacade.getChildrenData(eq(yesterday), eq(yesterday), any(URL.class), eq(15L)))
                 .thenReturn(childDs2);
+        // mothers
+        when(mctsWebServiceFacade.getMothersData(eq(yesterday), eq(yesterday), any(URL.class), eq(1L)))
+                .thenReturn(motherDs1);
+        when(mctsWebServiceFacade.getMothersData(eq(yesterday), eq(yesterday), any(URL.class), eq(15L)))
+                .thenReturn(motherDs2);
 
         mctsWsImporter.handleImportEvent(new MotechEvent());
 
@@ -154,6 +162,14 @@ public class MctsWsImporterTest {
         verifyChild(mapCaptor.getAllValues().get(1), 1, 1);
         verifyChild(mapCaptor.getAllValues().get(2), 2, 15);
         verifyChild(mapCaptor.getAllValues().get(3), 3, 15);
+
+        // mothers
+        mapCaptor = ArgumentCaptor.forClass(Map.class);
+        verify(mctsBeneficiaryImportService, times(4)).importMotherRecord(mapCaptor.capture());
+        verifyMother(mapCaptor.getAllValues().get(0), 0, 1);
+        verifyMother(mapCaptor.getAllValues().get(1), 1, 1);
+        verifyMother(mapCaptor.getAllValues().get(2), 2, 15);
+        verifyMother(mapCaptor.getAllValues().get(3), 3, 15);
     }
 
     private void prepFlwData() {
@@ -215,6 +231,19 @@ public class MctsWsImporterTest {
         childDs2.setRecords(asList(child3, child4));
     }
 
+    private void prepMotherData() {
+        MotherRecord mother1 = createMother(0, 1);
+        MotherRecord mother2 = createMother(1, 1);
+        MotherRecord mother3 = createMother(2, 15);
+        MotherRecord mother4 = createMother(3, 15);
+
+        motherDs1 = new MothersDataSet();
+        motherDs1.setRecords(asList(mother1, mother2));
+
+        motherDs2 = new MothersDataSet();
+        motherDs2.setRecords(asList(mother3, mother4));
+    }
+
     private ChildRecord createChild(long id, long stateId) {
         ChildRecord record = new ChildRecord();
 
@@ -256,7 +285,7 @@ public class MctsWsImporterTest {
         assertEquals(id + 2, record.get(KilkariConstants.PHC));
         assertEquals(id + 3 , record.get(KilkariConstants.SUBCENTRE));
         assertEquals(id + 4, record.get(KilkariConstants.CENSUS_VILLAGE));
-        assertEquals("Child "+ id, record.get(KilkariConstants.BENEFICIARY_NAME));
+        assertEquals("Child " + id, record.get(KilkariConstants.BENEFICIARY_NAME));
         assertEquals(Long.valueOf("48" + id), record.get(KilkariConstants.MSISDN));
         assertEquals(today.toDateTime(new LocalTime(10, (int) id)), record.get(KilkariConstants.DOB));
 
@@ -267,8 +296,66 @@ public class MctsWsImporterTest {
         assertEquals(String.valueOf(id + 5), mother.getBeneficiaryId());
     }
 
-    private void prepEmtyImport() {
-        when(mctsWebServiceFacade.getMothersData(any(LocalDate.class), any(LocalDate.class), any(URL.class),
-                any(Long.class))).thenReturn(new MothersDataSet());
+    private MotherRecord createMother(long id, long stateId) {
+        MotherRecord mother = new MotherRecord();
+
+        mother.setStateId(stateId);
+        mother.setDistrictId(stateId + 1);
+        mother.setTalukaId("Taluka " + id);
+        mother.setHealthBlockId(id + 1);
+        mother.setPhcid(id + 2);
+        mother.setSubCentreid(id + 3);
+        mother.setVillageId(id + 4);
+
+        String motherId = String.valueOf(id);
+        mother.setIdNo(motherId);
+        MctsMother mctsMother = new MctsMother(motherId);
+        when(mctsBeneficiaryValueProcessor.getOrCreateMotherInstance(motherId)).thenReturn(mctsMother);
+
+        mother.setName("Mother " + id);
+
+        mother.setWhomPhoneNo("49" + id);
+        when(mctsBeneficiaryValueProcessor.getMsisdnByString("49" + id)).thenReturn(Long.valueOf("49" + id));
+
+        DateTime lmp = today.toDateTime(new LocalTime(15, (int) id));
+        mother.setLmpDate(lmp.toString());
+        when(mctsBeneficiaryValueProcessor.getDateByString(lmp.toString())).thenReturn(lmp);
+
+        DateTime dob = today.toDateTime(new LocalTime(16, (int) id));
+        mother.setBirthdate(dob.toString());
+        when(mctsBeneficiaryValueProcessor.getDateByString(dob.toString())).thenReturn(dob);
+
+        mother.setAbortion(motherId);
+        when(mctsBeneficiaryValueProcessor.getAbortionDataFromString(motherId)).thenReturn(id % 2 == 0);
+
+        mother.setOutcomeNos((int) id);
+        when(mctsBeneficiaryValueProcessor.getStillBirthFromString(motherId)).thenReturn(id % 2 == 0);
+
+        mother.setEntryType((int) id);
+        when(mctsBeneficiaryValueProcessor.getDeathFromString(motherId)).thenReturn(id % 2 == 0);
+
+        return mother;
+    }
+
+    private void verifyMother(Map<String, Object> record, long id, long stateId) {
+        assertEquals(stateId, record.get(KilkariConstants.STATE));
+        assertEquals(stateId + 1, record.get(KilkariConstants.DISTRICT));
+        assertEquals("Taluka " + id, record.get(KilkariConstants.TALUKA));
+        assertEquals(id + 1, record.get(KilkariConstants.HEALTH_BLOCK));
+        assertEquals(id + 2, record.get(KilkariConstants.PHC));
+        assertEquals(id + 3, record.get(KilkariConstants.SUBCENTRE));
+        assertEquals(id + 4, record.get(KilkariConstants.CENSUS_VILLAGE));
+
+        MctsMother mctsMother = (MctsMother) record.get(KilkariConstants.BENEFICIARY_ID);
+        assertEquals(String.valueOf(id), mctsMother.getBeneficiaryId());
+
+        assertEquals("Mother " + id, record.get(KilkariConstants.BENEFICIARY_NAME));
+        assertEquals(Long.valueOf("49" + id), record.get(KilkariConstants.MSISDN));
+        assertEquals(today.toDateTime(new LocalTime(15, (int) id)), record.get(KilkariConstants.LMP));
+        assertEquals(today.toDateTime(new LocalTime(16, (int) id)), record.get(KilkariConstants.MOTHER_DOB));
+        assertEquals(id % 2 == 0, record.get(KilkariConstants.ABORTION));
+        assertEquals(id % 2 == 0, record.get(KilkariConstants.STILLBIRTH));
+        assertEquals(id % 2 == 0, record.get(KilkariConstants.DEATH));
+
     }
 }
