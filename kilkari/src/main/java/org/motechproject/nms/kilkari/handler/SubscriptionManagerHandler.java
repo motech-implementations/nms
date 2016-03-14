f4ff4500a5064d86a4f6606d2a97e0e2e0b3fa1f
Koshal Thirumalai <kosh@koshal.org>
2016-03-14 16:34:53 -0700

    Removing transactional from listener

--- kilkari/src/main/java/org/motechproject/nms/kilkari/handler/SubscriptionManagerHandler.java
+++ kilkari/src/main/java/org/motechproject/nms/kilkari/handler/SubscriptionManagerHandler.java
@@ -16,7 +16,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.PostConstruct;
 
