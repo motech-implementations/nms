0ef93fb7403ce1c4e5e14d673fa5ce3f786841e4
haritha <haritha@beehyv.com>
2018-08-27 15:40:37 +0530

    null pointer for couple of children - added null cehcks on rchid

--- kilkari/src/main/java/org/motechproject/nms/kilkari/service/impl/SubscriptionServiceImpl.java
+++ kilkari/src/main/java/org/motechproject/nms/kilkari/service/impl/SubscriptionServiceImpl.java
@@ -340,15 +340,15 @@ public class SubscriptionServiceImpl implements SubscriptionService {
                     subscriptionsSize = subscriptions.size();
                     if (subscriptionsSize != 0) {
                         if (subscriptionsSize == 1) {
-                            if (SubscriptionPackType.PREGNANCY.equals(subscriptions.get(0).getSubscriptionPack().getType()) && subscriber.getMother() != null && !subscriber.getMother().getRchId().equals(motherRchId)) {
+                            if (SubscriptionPackType.PREGNANCY.equals(subscriptions.get(0).getSubscriptionPack().getType()) && subscriber.getMother() != null && subscriber.getMother().getRchId() != null && !subscriber.getMother().getRchId().equals(motherRchId)) {
                                 return true;
-                            } else if (SubscriptionPackType.PREGNANCY.equals(subscriptions.get(0).getSubscriptionPack().getType()) && subscriber.getMother() != null && subscriber.getMother().getRchId().equals(motherRchId)) {
+                            } else if (SubscriptionPackType.PREGNANCY.equals(subscriptions.get(0).getSubscriptionPack().getType()) && subscriber.getMother() != null && subscriber.getMother().getRchId() != null && subscriber.getMother().getRchId().equals(motherRchId)) {
                                 return false;
                             } else {
                                 return (subscriber.getChild() != null && subscriber.getChild().getRchId() != null && !subscriber.getChild().getRchId().equals(childRchId));
                             }
                         } else {
-                            return ((subscriber.getChild() != null && !subscriber.getChild().getRchId().equals(childRchId)) || (subscriber.getMother() != null && !subscriber.getMother().getRchId().equals(motherRchId)));
+                            return ((subscriber.getChild() != null && subscriber.getChild().getRchId() !=null &&   !subscriber.getChild().getRchId().equals(childRchId)) || (subscriber.getMother() != null && subscriber.getMother().getRchId() != null && !subscriber.getMother().getRchId().equals(motherRchId)));
                         }
                     }
                 }
