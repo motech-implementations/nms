c250129174343dced41a70c576473d6c09b93c49
gudipatiharitha <haritha@beehyv.com>
2018-10-24 01:37:21 +0530

    DB Script to be run automatically before test cases

--- AutoIncrementIDsScript.sql
+++ AutoIncrementIDsScript.sql
@@ -1,33 +1,18 @@
-Please run the following queries in the specified order in case of first time deployment:
-
+use motech_data_services;
 SET FOREIGN_KEY_CHECKS=0;
-
 ALTER TABLE `nms_districts` CHANGE COLUMN `id` `id` BIGINT(20) NOT NULL AUTO_INCREMENT ;
-
 ALTER TABLE `nms_talukas` CHANGE COLUMN `id` `id` BIGINT(20) NOT NULL AUTO_INCREMENT ;
-
 ALTER TABLE `nms_health_blocks` CHANGE COLUMN `id` `id` BIGINT(20) NOT NULL AUTO_INCREMENT;
-
 ALTER TABLE `nms_health_facilities` CHANGE COLUMN `id` `id` BIGINT(20) NOT NULL AUTO_INCREMENT;
-
 ALTER TABLE `nms_health_sub_facilities` CHANGE COLUMN `id` `id` BIGINT(20) NOT NULL AUTO_INCREMENT;
-
 ALTER TABLE `nms_villages` CHANGE COLUMN `id` `id` BIGINT(20) NOT NULL AUTO_INCREMENT;
-
 ALTER TABLE `nms_mother_rejects` CHANGE COLUMN `id` `id` BIGINT(20) NOT NULL AUTO_INCREMENT;
-
 ALTER TABLE `nms_child_rejects` CHANGE COLUMN `id` `id` BIGINT(20) NOT NULL AUTO_INCREMENT;
-
 ALTER TABLE `nms_imi_cdrs` CHANGE COLUMN `id` `id` BIGINT(20) NOT NULL AUTO_INCREMENT ;
-
 ALTER TABLE `nms_imi_csrs` CHANGE COLUMN `id` `id` BIGINT(20) NOT NULL AUTO_INCREMENT ;
-
-ALTER TABLE `motech_data_services`.`nms_taluka_healthblock`
+ALTER TABLE `motech_data_services`.`nms_taluka_healthblock`;
 CHANGE COLUMN `id` `id` BIGINT(20) NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE `motech_data_services`.`nms_village_healthsubfacility`
+ALTER TABLE `motech_data_services`.`nms_village_healthsubfacility`;
 CHANGE COLUMN `id` `id` BIGINT(20) NOT NULL AUTO_INCREMENT;
-
-
 SET FOREIGN_KEY_CHECKS=1;
 
