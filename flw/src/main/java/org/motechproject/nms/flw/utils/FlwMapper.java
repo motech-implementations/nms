993690f4e0d427dde2a6ed0fbcb34b6e4c0e491c
VishnuVimal <vishnu.vimal92@yahoo.in>
2019-01-08 13:51:39 +0530

    minor asha location bug fix

--- flw/src/main/java/org/motechproject/nms/flw/utils/FlwMapper.java
+++ flw/src/main/java/org/motechproject/nms/flw/utils/FlwMapper.java
@@ -96,7 +96,7 @@ public final class FlwMapper {
             flw.setJobStatus(jobStatus);
         }
 
-        if (FlwJobStatus.ACTIVE.equals(gfStatus)) {
+        if (FlwJobStatus.ACTIVE.toString().equalsIgnoreCase(gfStatus)) {
             setFrontLineWorkerLocation(flw, location);
         }
 
