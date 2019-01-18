a20e69e4ff2763c66c76e62997ffebc0956e2f88
VishnuVimal <vishnu.vimal92@yahoo.in>
2019-01-18 17:06:49 +0530

    logs for file already present for state and date rch

--- rch/src/main/java/org/motechproject/nms/rch/service/impl/RchImportFacilitatorServiceImpl.java
+++ rch/src/main/java/org/motechproject/nms/rch/service/impl/RchImportFacilitatorServiceImpl.java
@@ -37,7 +37,7 @@ public class RchImportFacilitatorServiceImpl implements RchImportFacilitatorServ
         if (rchImportFacilitator.getFileName() == null) {
             throw new RchFileManipulationException("Invalid file name");
         } else if (rchImportFacilitator1.size() != 0) {
-            LOGGER.error("A record already present for the same state and today's date.");
+            LOGGER.debug("A record already present for the same state: {} and today's date: {}.", state, importDate);
         } else {
             rchImportFacilitatorDataService.create(rchImportFacilitator);
         }
