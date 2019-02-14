a2202bc6ff04a6c548feefd0c5dc5ea02bb27746
VishnuVimal <vishnu.vimal92@yahoo.in>
2019-02-14 16:05:20 +0530

    constraintviolation check

--- kilkari/src/main/java/org/motechproject/nms/kilkari/service/impl/DeactivatedBeneficiaryServiceImpl.java
+++ kilkari/src/main/java/org/motechproject/nms/kilkari/service/impl/DeactivatedBeneficiaryServiceImpl.java
@@ -38,7 +38,7 @@ public class DeactivatedBeneficiaryServiceImpl implements DeactivatedBeneficiary
             }
             return deactivatedBeneficiaryWithSelectDeactivationReasons;
         } catch (ConstraintViolationException e) {
-            LOGGER.debug("List of constraints: {}", e.getConstraintViolations());
+            LOGGER.error("List of constraints: {}", e.getConstraintViolations());
             throw e;
         }
     }
