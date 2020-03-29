a64dfc6b5dbdb25918a8a9a5e82ad5ab2e8e1b29
ValliC444 <srivalli@beehyv.com>
2020-03-30 04:33:37 +0530

    ratelimiting (in progress)

--- imi/src/main/java/org/motechproject/nms/imi/web/WebConfig.java
+++ imi/src/main/java/org/motechproject/nms/imi/web/WebConfig.java
@@ -1,28 +1,29 @@
 package org.motechproject.nms.imi.web;
 
-import io.github.bucket4j.Bandwidth;
-import io.github.bucket4j.Bucket;
-import io.github.bucket4j.Bucket4j;
-import io.github.bucket4j.Refill;
+//import io.github.bucket4j.Bandwidth;
+//import io.github.bucket4j.Bucket;
+//import io.github.bucket4j.Bucket4j;
+//import io.github.bucket4j.Refill;
 
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import java.time.Duration;
+//import java.time.Duration;
 
 public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        Refill refill = Refill.intervally(1, Duration.ofMinutes(1));
-        Bandwidth limit = Bandwidth.classic(1, refill).withInitialTokens(1);
-        Bucket bucket = Bucket4j.builder().addLimit(limit).build();
-        registry.addInterceptor(new RateLimitInterceptor(bucket, 1)).addPathPatterns("/generateTargetFile");
+//        Refill refill = Refill.intervally(1, Duration.ofMinutes(1));
+//        Bandwidth limit = Bandwidth.classic(1, refill).withInitialTokens(1);
+//        Bucket bucket = Bucket4j.builder().addLimit(limit).build();
+        registry.addInterceptor(new RateLimitInterceptor()).addPathPatterns("/generateTargetFile");
+//
+//        refill = Refill.intervally(2, Duration.ofMinutes(1));
+//        limit = Bandwidth.classic(2, refill);
+//        bucket = Bucket4j.builder().addLimit(limit).build();
+//        registry.addInterceptor(new RateLimitInterceptor(bucket, 1))
+//                .addPathPatterns("/cdrFileNotification");
 
-        refill = Refill.intervally(2, Duration.ofMinutes(1));
-        limit = Bandwidth.classic(2, refill);
-        bucket = Bucket4j.builder().addLimit(limit).build();
-        registry.addInterceptor(new RateLimitInterceptor(bucket, 1))
-                .addPathPatterns("/cdrFileNotification");
     }
 }
