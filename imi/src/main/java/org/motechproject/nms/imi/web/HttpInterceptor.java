1edfa99a7fd8ca54e8dcb9a8c1119e4d5d1205ca
ValliC444 <srivalli@beehyv.com>
2020-03-29 10:02:42 +0530

    ratelimiting (in progress)

---
+++ imi/src/main/java/org/motechproject/nms/imi/web/HttpInterceptor.java
@@ -0,0 +1,15 @@
+package org.motechproject.nms.imi.web;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HttpInterceptor implements HandlerInterceptor {
+        @Override
+        public boolean preHandle(HttpServletRequest request,
+                                 HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("Inside pre handle");
+        return true;
+    }
+
+    }
