fe3cd7a9bba3e95de006b9bfef0e07e6a0350dc5
ValliC444 <srivalli@beehyv.com>
2020-03-30 05:12:47 +0530

    ratelimiting (in progress)

--- imi/src/main/java/org/motechproject/nms/imi/web/RateLimitInterceptor.java
+++ imi/src/main/java/org/motechproject/nms/imi/web/RateLimitInterceptor.java
@@ -1,42 +1,40 @@
 package org.motechproject.nms.imi.web;
-//import io.github.bucket4j.Bucket;
-//import io.github.bucket4j.ConsumptionProbe;
+import io.github.bucket4j.*;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
 
-//import java.util.concurrent.TimeUnit;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public class RateLimitInterceptor implements HandlerInterceptor {
 //
-//    private final Bucket bucket;
-//
-//    private final int numTokens;
+    private final Bucket bucket;
 
-//    public RateLimitInterceptor(Bucket bucket, int numTokens) {
-//        this.bucket = bucket;
-//        this.numTokens = numTokens;
-//    }
+    public RateLimitInterceptor(int capacity) {
+        Refill refill = Refill.intervally(capacity, Duration.ofMinutes(1));
+        Bandwidth limit = Bandwidth.classic(capacity, refill);
+        this.bucket = Bucket4j.builder().addLimit(limit).build();
+    }
 
     @Override
     public boolean preHandle(HttpServletRequest request,
                                  HttpServletResponse response, Object handler) throws Exception {
         System.out.println("Inside pre handle");
-//        ConsumptionProbe probe = this.bucket.tryConsumeAndReturnRemaining(this.numTokens);
-//        if (probe.isConsumed()) {
-//            response.addHeader("X-Rate-Limit-Remaining",
-//                    Long.toString(probe.getRemainingTokens()));
-//            return true;
-//        }
+        ConsumptionProbe probe = this.bucket.tryConsumeAndReturnRemaining(1);
+        if (probe.isConsumed()) {
+            response.addHeader("X-Rate-Limit-Remaining",
+                    Long.toString(probe.getRemainingTokens()));
+            return true;
+        }
 //
-//        response.setStatus(429); // 429
-//        response.addHeader("X-Rate-Limit-Retry-After-Milliseconds",
-//                Long.toString(TimeUnit.NANOSECONDS.toMillis(probe.getNanosToWaitForRefill())));
-//
-//        return false;
-        return true;
+        response.setStatus(429); // 429
+        response.addHeader("X-Rate-Limit-Retry-After-Milliseconds",
+                Long.toString(TimeUnit.NANOSECONDS.toMillis(probe.getNanosToWaitForRefill())));
+
+        return false;
     }
 
     @Override
