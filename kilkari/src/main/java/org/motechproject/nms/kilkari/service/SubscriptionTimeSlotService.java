33d68e157ea6e3ef15ae289a79d07468e6a8bb18
Kantas2601 <kamal.k@beehyv.com>
2024-09-25 18:43:52 +0530

    Time Slot optimization column in OBD files

---
+++ kilkari/src/main/java/org/motechproject/nms/kilkari/service/SubscriptionTimeSlotService.java
@@ -0,0 +1,11 @@
+package org.motechproject.nms.kilkari.service;
+
+import org.motechproject.nms.kilkari.domain.SubscriptionTimeSlot;
+
+import java.util.List;
+
+public interface SubscriptionTimeSlotService {
+
+    List<SubscriptionTimeSlot> findTimeSlotsForSubscriptionsById(List<String> subscriptionId);
+
+}
