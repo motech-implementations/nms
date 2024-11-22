edab2378e5533a5488143695e1d3665f40517d1c
Mandar-Beehyv <sanjay.mandar@beehyv.com>
2024-11-22 16:05:00 +0530

    fixed two subscription case

--- kilkari/src/main/java/org/motechproject/nms/kilkari/service/impl/SubscriberServiceImpl.java
+++ kilkari/src/main/java/org/motechproject/nms/kilkari/service/impl/SubscriberServiceImpl.java
@@ -372,7 +372,12 @@ public class SubscriberServiceImpl implements SubscriberService {
         } else { // subscriberByBeneficiary != null aka. RCH mother exists in motech
             if (subscribersByMsisdn.isEmpty()) {  //no subscriber attached to the new number
                 // We got here because beneficiary's phone number changed
-                if (subscriberByRchId.getCaseNo() == null) {
+                Subscription childSubscription = subscriptionService.getActiveSubscription(subscriberByRchId, SubscriptionPackType.CHILD);
+                if(childSubscription != null){
+                    subscriptionErrorDataService.create(new SubscriptionError(msisdn, motherUpdate.getRchId(), SubscriptionRejectionReason.ALREADY_SUBSCRIBED, pack.getType(), "Active subscription exists with different caseNo", SubscriptionOrigin.RCH_IMPORT));
+                    return null;
+                }
+                else if (subscriberByRchId.getCaseNo() == null) {
                     subscriberByRchId.setCaseNo(caseNo);
                     motherUpdate.setMaxCaseNo(caseNo);
                 } else if (subscriberByRchId.getCaseNo() > caseNo) {
