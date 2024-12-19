98929ea7e54a1c74b23b8824d54bc1058d01d70d
dinesh.c <dinesh.c@beehyv.com>
2024-12-19 12:41:01 +0530

    Target file performance Improvement.

--- kilkari/src/main/java/org/motechproject/nms/kilkari/utils/KilkariConstants.java
+++ kilkari/src/main/java/org/motechproject/nms/kilkari/utils/KilkariConstants.java
@@ -147,6 +147,11 @@ public final class KilkariConstants {
     public static final String CHILD_REGISTRATION_DATE = "ChildRegistrationDate";
     public  static final String WP_STATES = "rch.wp.states";
 
+
+    public static final String DB_URL_JDBC = "db.url";
+    public static final String DB_USER_JDBC = "db.user";
+    public static final String DB_PASSWORD_JDBC = "db.password";
+
     private KilkariConstants() {
     }
 }
